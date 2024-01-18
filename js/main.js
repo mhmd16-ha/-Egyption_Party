39d6c11ca09c34e8093db3c83aea9584123c7fcc
mhmd16-ha <mhmdhmooda44@gmail.com>
2024-01-19 00:39:59 +0200

    add accordion section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $("#slide i").click(function(){
     $("#slide").animate({width:'0'},200)
+    $("#homeItem").animate({marginLeft:'0px'},200)
     // console.log("fff");
 })
 $("#open").click(function(){
     $("#slide").animate({width:'250px'},200)
+    $("#homeItem").animate({marginLeft:'250px'},200)
     // $("#slide a ").fadeIn(100)
+})
+$(".singer").click(function(){
+    $('.singerText').not($(this).next()).slideUp(500);
+    $(this).next().slideToggle(500)
 })
