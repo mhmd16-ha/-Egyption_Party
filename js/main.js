1a72de8ca74e7ad53105ec9c8504da66cbf40466
mhmd16-ha <mhmdhmooda44@gmail.com>
2024-01-18 22:13:32 +0200

    Home section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$("#slide i").click(function(){
+    $("#slide").animate({width:'0'},200)
+    // console.log("fff");
+})
+$("#open").click(function(){
+    $("#slide").animate({width:'25%'},200)
+    // $("#slide a ").fadeIn(100)
+})
